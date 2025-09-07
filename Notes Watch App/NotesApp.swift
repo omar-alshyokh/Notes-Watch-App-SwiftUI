dc09412cd094b9207978bcec82abebfbd85dd608
OmMAar <omar.alshyokh@gmail.com>
2025-09-07 16:05:26 +0400

    first commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Omar Alshyokh on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
